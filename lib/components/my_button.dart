17c4840f51dca81097ee2e71fdc4dc77710efa01
mozidan27 <153581941+mozidan27@users.noreply.github.com>
2024-05-19 08:31:45 +0300

    login page , my button , and text field created succ

---
+++ lib/components/my_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final void Function()? onTap;
+  final String text;
+  const MyButton({super.key, this.onTap, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 25),
+        padding: const EdgeInsets.all(23),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).colorScheme.inversePrimary,
+                fontSize: 18),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,12 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:zchat/components/my_button.dart';
+import 'package:zchat/components/my_text_field.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _pWController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // logo or image
+            Icon(
+              Icons.chat,
+              size: 64,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+
+            const SizedBox(
+              height: 20,
+            ),
+
+            // welcome text
+            Text(
+              'Welcome back you\'ve been missed!',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+
+            // email box
+            MyTextField(
+                controller: _emailController,
+                hintText: 'Email',
+                obscureText: false),
+            const SizedBox(
+              height: 10,
+            ),
+
+            // PW box
+            MyTextField(
+                controller: _pWController,
+                hintText: 'Password',
+                obscureText: true),
+            const SizedBox(
+              height: 30,
+            ),
+
+            // login button
+            const MyButton(text: 'Login'),
+
+            const SizedBox(
+              height: 30,
+            ),
+
+            // register now
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Not a memeber? ',
+                  style:
+                      TextStyle(color: Theme.of(context).colorScheme.primary),
+                ),
+                Text(
+                  'Register now',
+                  style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
