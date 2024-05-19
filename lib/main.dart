f047f1aa462332f983695e4d312849b410b4a6d7
mozidan27 <153581941+mozidan27@users.noreply.github.com>
2024-05-19 08:07:51 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:zchat/pages/login_page.dart';
+import 'package:zchat/theme/light_mode.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const LoginPage(),
+      theme: lightMode,
+    );
+  }
+}
