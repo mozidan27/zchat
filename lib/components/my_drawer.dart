c056a0218be28e978c8a9d4a967d50ae16dafa0b
mozidan27 <153581941+mozidan27@users.noreply.github.com>
2024-05-20 07:35:36 +0300

    created setting page and the drawer

---
+++ lib/components/my_drawer.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:zchat/auth/auth_service.dart';
+import 'package:zchat/pages/setting_page.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+  void logout() {
+    // get auth service
+    final auth = AuthService();
+
+    // sign out
+    auth.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              // drawer header will put my logo or icon
+
+              DrawerHeader(
+                child: Icon(
+                  size: 54,
+                  Icons.chat,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+
+              // list tile for home
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: const Text("H O M E"),
+                  leading: const Icon(Icons.home),
+                  onTap: () => Navigator.pop(context),
+                ),
+              ),
+
+              // list tile for settings
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: const Text("S E T T I N G S"),
+                  leading: const Icon(Icons.settings),
+                  onTap: () {
+                    // pop the drawer
+                    Navigator.pop(context);
+
+                    // navigate to setting page
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SettingPage(),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+          // list tile for logout putton
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, bottom: 30),
+            child: ListTile(
+              title: const Text("L O G O U T"),
+              leading: const Icon(Icons.logout),
+              onTap: logout,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,31 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:zchat/auth/auth_service.dart';
+import 'package:zchat/components/my_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  // logout method
-
-  void logout() {
-    // get auth service
-    final auth = AuthService();
-
-    // sign out
-    auth.signOut();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home Page"),
-        actions: [
-          IconButton(
-            onPressed: logout,
-            icon: const Icon(Icons.logout),
-          ),
-        ],
       ),
+      drawer: const MyDrawer(),
     );
   }
 }
