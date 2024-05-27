dc7755397388f72f6718c6f5cc69bb6d2c3e2dae
mozidan27 <153581941+mozidan27@users.noreply.github.com>
2024-05-27 06:11:48 +0300

    chat services class, get instance and get user methods created

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:zchat/components/my_drawer.dart';
+import 'package:zchat/services/auth/auth_service.dart';
+import 'package:zchat/services/chat/chat_services.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  // chat and auth services
+
+  final ChatService _chatService = ChatService();
+  final AuthService _authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
